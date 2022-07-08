da36b119a731a4031814d46e320113fd24ea3f2f
JArcher <viewtiful.kja@gmail.com>
2022-07-08 19:53:11 -0400

    Pulling down changes

--- js/script.js
+++ js/script.js
@@ -12,13 +12,13 @@ var questions =
 
             choices: [{ text: "Action", param: "28" }, { text: "Adventure", param: "12" }, { text: "Animation", param: "16" }, { text: "Comedy", param: "35" }, { text: "Crime", param: "80" }, { text: "Documentary", param: "99" }, { text: "Drama", param: "18" }, { text: "Family", param: "10751" }, { text: "Fantasy", param: "14" }, { text: "History", param: "36" }, { text: "Horror", param: "27" }, { text: "Music", param: "10402" }, { text: "Mystery", param: "9648" }, { text: "Romance", param: "10749" }, { text: "Science Fiction", param: '878' }, { text: "Thriller", param: "53" }, { text: "TV Movie", param: "10770" }, { text: "War", param: "10752" }, { text: "Western", param: "37" }],
 
-            choices: ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "History", "Horror", "Music", "Mystery", "Romance", "Science Fiction", "Thriller", "TV Movie", "War", "Western"],
+
 
 
         },
         {
-            question: "What is your favorite movie genre?",
-            choices: ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "History", "Horror", "Music", "Mystery", "Romance", "Science Fiction", "Thriller", "TV Movie", "War", "Western"],
+            question: "What is your favorite movie genre?"//,
+            //choices: [{text: , param: },
 
         }
 
@@ -27,12 +27,13 @@ var questions =
     ];
 
 
-
-
 /*Container variables*/
 
-//const strtBtn = ;
+const strtBtn = $('#strtBtn');
+const prevBtn = $('#prev').hide();
+const nextBtn = $('#next').hide();
 const questionCont = document.getElementById('questionaire-div');
+const qCont = $('questionaire-div');
 const questionDiv = document.getElementById('question');
 const answerDiv = document.getElementById('answer');
 
@@ -45,60 +46,84 @@ function storeAnswer(val) {
 
 
 
-/**Test Button */
-var test = document.createElement('input')
-test.value = 'test';
-test.type = "button";
-test.class = 'waves-effect waves-light btn-large';
-test.id = 't';
-questionCont.appendChild(test)
-test.addEventListener('click', storeAnswer);
+/**Generates Button for answers */
+function genBtn(test) {
+    event.stopPropagation()
+    qCont.empty();
+    var choiceBtn = document.createElement('input');
+    choiceBtn.className = 'waves-effect waves-light btn-large';
+    choiceBtn.id = 'choice' + '-' + choiceBtn.param;
+    choiceBtn.innerHTML = test.text;
+    choiceBtn.value = test.text;
+    choiceBtn.type = "button";
+    questionDiv.appendChild(choiceBtn);
+    choiceBtn.addEventListener('click', storeAnswer);
+
+}
 
 
-
-/*API Configuration - Gets a random activity from /*    
 
 /*Get Question from questions object */
-var questionObj = function getQuestion(qIndex) {
+function questionObj(qIndex) {
+    event.preventDefault()
     createQuestion = document.createElement('h2');
     var q = questions[qIndex].question;
 
     createQuestion.innerText = q;
     questionDiv.appendChild(createQuestion);
     var a = questions[qIndex].choices.forEach(answer => {
-        var createBtn = document.createElement('button')
-        createBtn.innerText = answer.text;
-        createBtn.addEventListener('click', console.log('test'))
+        genBtn(answer);
     });
     //return q, a;
 }
 
 /*Generate Questions*/
 function showQuestion() {
-    //$(this).hide();
-    //$(this).show();
     let qIndex = 0;
+    prevBtn.show();
+    nextBtn.show();
+    strtBtn.hide();
+    //$(this).show();
+
     let qPrompt = questionObj(qIndex);
-    console.log("test " + qPrompt);
+    prevBtn.on('click', function () { prevQ(qIndex) });
+    nextBtn.on('click', function () { nextQ(qIndex) });
+    console.log(q)
+}
+
+/* Function for Next Button */
+function nextQ(indx) {
+    if (indx < questions.length) {
+        indx++;
+        qCont.empty();
+        questionObj(indx);
+    }
+}
+
+/* Function for Prev Button */
+function prevQ(indx) {
+    if (indx < 0) {
+        indx--;
+        qCont.empty();
+        questionObj(indx);
+    }
 }
 
 
-
-
 /* API Configuration - Gets movie data and stores it in a function */
-var getMovieData = function fetchMovieData(mLength, mGenre, mAge) {
+/*var getMovieData = function fetchMovieData(mLength, mGenre, mAge) {
     fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d68384526c8f6fabc89f85ba5e6c3f5a&language=en-USZ&with_genres=${mGenre}&${mLength}=90&sort_by=vote_average.desc&vote_count.gte=10`)
         .then((response) => { return response.json() })
         .then((data) => {
 
             /**Stores top 3 results in variable */
 
-            movieRes = data.results.slice(0, 3);
+/*movieRes = data.results.slice(0, 3);
 
-            return console.log(movieRes);
-            //,console.log(data.results[0].backdrop_path);
+return console.log(movieRes);
+//,console.log(data.results[0].backdrop_path);
 
-    ]
+]*/
 
 /* API Configuration - Gets movie data and stores it in a function */
 var getMovieData = function fetchMovieData(mLength, mGenre, mAge) {
@@ -114,13 +139,27 @@ var getMovieData = function fetchMovieData(mLength, mGenre, mAge) {
 }
 
 
+/*Bored API Configuration - Gets a random activity from */
+var getRanAct = function () {
+    fetch('https://www.boredapi.com/api/activity/')
+        .then(function (response) { return response.json(); })
+        .then(function (data) { return console.log(data.activity) });
+}
+
+
 
 //test api call
 //getMovieData("with_runtime.lte", [14, 12, 35]);
 
 //questionObj(0);
 //showQuestion();
-questionObj(0);
+//questionObj(0);
 
 //console.log(movieData);
-getMovieData();
+//getMovieData();
+
+//getRanAct();
+
+//genBtn();
+strtBtn.on('click', showQuestion);
+console.log(questions.length);
